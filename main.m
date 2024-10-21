64afda621d07bc55a34a7cb247e59923836ba610
Reeyan <rchoud29@asu.edu>
2024-10-21 16:36:43 -0700

    Fixed Driving. Added Lift Controls

--- main.m
+++ main.m
@@ -1,20 +1,45 @@
 global key
 global brick % assume brick has already been initialized with brick = ConnectBrick("8k")
 InitKeyboard();
+
+speed_mulltiplier = 1;
+
+brick.MoveMotor('A', 0)
+brick.MoveMotor('B', 0)
+brick.MoveMotor('C', 0)
+brick.MoveMotor('D', 0)
 while 1
     pause(0.1)
     switch key
+        case 'i'
+            speed_multiplier = 2;
+        case 'w'
+            brick.MoveMotor('A', 50 * speed_mulltiplier)
+            brick.MoveMotor('D', 50 * speed_multiplier)
+        case 's'
+            brick.MoveMotor('A', -50 * speed_multiplier)
+            brick.MoveMotor('D', -50 * speed_multiplier)
+        case 'd'
+            brick.MoveMotor('A', 50 * speed_multiplier)
+            brick.MoveMotor('D', -50 * speed_multiplier)
+        case 'a'
+            brick.MoveMotor('A', -50 * speed_multiplier)
+            brick.MoveMotor('D', 50 * speed_multiplier)
+        case 'e'
+            brick.MoveMotorAngleRel('B', 50 * speed_multiplier, 1000)
+        case 'r'
+            brick.MoveMotorAngleRel('B', -50 * speed_multiplier, 1000)
         case 'q' % kill switch
-            brick.MoveMotor('1', 0)
-            brick.MoveMotor('2', 0)
-            brick.MoveMotor('3', 0)
-            brick.MoveMotor('4', 0)
             brick.MoveMotor('A', 0)
             brick.MoveMotor('B', 0)
             brick.MoveMotor('C', 0)
             brick.MoveMotor('D', 0)
-
             break;
+        otherwise
+            brick.MoveMotor('A', 0)
+            brick.MoveMotor('B', 0)
+            brick.MoveMotor('C', 0)
+            brick.MoveMotor('D', 0)
     end
 end
 
